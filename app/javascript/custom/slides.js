6d1cdc919c1643886050ba16fe7372e432ad8cf5
Sokolovskii58 <arenda244@ya.ru>
2021-03-29 23:49:25 +0400

    Like mode & reset bootstrap version

---
+++ app/javascript/custom/slides.js
@@ -0,0 +1,36 @@
+$(function(){
+    var $activeSlide = $('#slides .slide:first-child');
+
+ //   $activeSlide.addClass("showing");
+
+    $(".match-people").on("click")
+
+    $("#decline").on("click", function(){
+        goToSlide('decline');
+    });
+
+    $("#approve").on("click", function(){
+        var user_id = $activeSlide.data("id");
+
+        $.ajax({
+            url: "/approve/" + user_id,
+            method: "post",
+            dataType: "ajax"
+        })
+        goToSlide('#approve');
+    });
+    
+    function goToSlide(action){
+        $activeSlide.removeClass("showing");
+        $activeSlide = $activeSlide.next(".slide");
+        
+        
+        if(action == "approve"){
+            
+        } else {
+            
+        }
+        
+        $activeSlide.addClass("showing");
+    }
+});
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
   devise_for :accounts
-	root to: "public#home"
+  
+
+  get "/browse" => "browse#browse", as: :browse
+  get "/matches" => "browse#matches", as: :matches
+  post "/approve/:id" => "browse#approve", as: :approve_user
+  post "/decline/:id" => "browse#decline", as: :decline_user
+
+  root to: "public#home"
 end
