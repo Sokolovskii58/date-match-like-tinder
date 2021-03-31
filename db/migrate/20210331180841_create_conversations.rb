650eb00059ea18fcce6035ca241337e3391df42c
Sokolovskii58 <arenda244@ya.ru>
2021-04-01 00:14:09 +0400

    Conversation and Message migrations

--- db/migrate/20210331180841_create_conversations.rb
+++ db/migrate/20210331180841_create_conversations.rb
@@ -1,8 +1,8 @@
 class CreateConversations < ActiveRecord::Migration[6.0]
   def change
     create_table :conversations do |t|
-      t.integer :account_1
-      t.integer :account_2
+      t.references :sender, null: false, references: :accounts
+      t.references :recipient, null: false, references: :accounts
 
 
       t.timestamps
